9829c07a975dd3b1753141806ee6f52498783ba8
dftseyaghuij <ksjcelvinteo095@hotmail.com>
2024-05-08 06:34:03 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const fibonacciRecursive = (n) => {
-  if (n <= 1) {
-    return n;
-  }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-};
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
