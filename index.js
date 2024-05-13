8d6baa48e90e52867133a91230b0d60d613d4220
dftseyaghuij <ksjcelvinteo095@hotmail.com>
2024-05-13 05:53:05 +0000

    aptos project

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-function rotateRight(head, k) {
-  if (!head) return null;
-  let length = 1;
-  let tail = head;
-  while (tail.next) {
-    length++;
-    tail = tail.next;
+function isValidBST(root) {
+  let prev = null;
+  return inorder(root);
+  function inorder(node) {
+    if (!node) return true;
+    if (!inorder(node.left)) return false;
+    if (prev !== null && node.val <= prev) return false;
+    prev = node.val;
+    return inorder(node.right);
   }
-  k %= length;
-  if (k === 0) return head;
-  let newTail = head;
-  for (let i = 0; i < length - k - 1; i++) {
-    newTail = newTail.next;
-  }
-  const newHead = newTail.next;
-  newTail.next = null;
-  tail.next = head;
-  return newHead;
 }
